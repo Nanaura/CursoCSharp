f15b626d674f220290080a94ad9364f472a591c8
ANA LAURA CLEMENTINO ALMEIDA <ana.lcalmeida1@senacsp.edu.br>
2023-05-24 16:47:06 -0300

    CursoCSharp

---
+++ ClassesEMetodos/ExerciciosMetodosComRetorno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class ExerciciosMetodosComRetorno
+    {
+        class RealToDolar
+        {
+            public double Converter()
+            {
+                Console.WriteLine("Informe o valor: ");
+                double.TryParse(Console.ReadLine(), out double real);
+                return real * 4.95;
+            }
+        }
+
+        class CelsiusToFahrenheit
+        {
+            public double Converter()
+            {
+                Console.WriteLine("Informe o valor: ");
+                double.TryParse(Console.ReadLine(), out double celsius);
+                return celsius * 9 / 5 + 32;
+            }
+        }
+
+        public static void Executar()
+        {
+            var realtodolar = new RealToDolar();
+            var convert = realtodolar.Converter();
+            Console.WriteLine(convert.ToString("F1"));
+            
+            var celsiusToFahrenheit = new CelsiusToFahrenheit();
+            var converter = celsiusToFahrenheit.Converter();
+            Console.WriteLine(convert);
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
+using CursoCSharp.ClassesEMetodos;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variaveis e Constantes - Fundamentos",VariaveisEConstantes.Executar },
+                {"Notação Ponto - Fundamentos ",NotacaoPonto.Executar },
+                {"Lendo Dados  - Fundamentos", LendoDados.Executar },
+                {"Formatando Numeros  - Fundamentos", FormatandoNumeros.Executar },
+                {"Operadores Aritmetricos  - Fundamentos", OperadoresAritmeticos.Executar },
+                {"Exercício 1 - Fundamentos", Exercicios.Executar },
+                {"Exercício IF e ELSE - Fundamentos", ExercicoIfElse.Executar },
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+                {"Exercicio Switch - Estruturas de Controle", ExercicioSwitch.Executar},
+                {"Estrutura While e For - Estruturas de Controle", EstruturaWhileEFor.Executar},
+                {"Exercicio While e For - Estruturas de Controle", ExercicioRepeticao.Executar},
+                {"Estrutura Foreach - Estruturas de Controle", EstruturaForeach.Executar},
+                {"Classes e Metodos - Classes e Metodos", Membros.Executar},
+                {"Construtor - Classes e Metodos", MetodosComRetorno.Executar},
+                {"Exercicio Metodos e Classes", ExerciciosMetodosComRetorno.Executar}
+
+
+            }); ;
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
